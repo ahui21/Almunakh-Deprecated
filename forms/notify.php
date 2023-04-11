25bfaad4538140c51032493283ce7dfd242a8abc
Alwin Hui <alwin.iwillwin@gmail.com>
2023-04-11 02:33:34 -0700

    fixed bug

--- forms/notify.php
+++ forms/notify.php
@@ -1,9 +1,11 @@
 <?php
-  $name - $_GET['name'];
-  $email = $_GET['email'];
+  $name = $_POST['name'];
+  $email = $_POST['email'];
   $to = 'alwin@almunakh.com';
   $subject = 'Notify me request';
   $headers = 'From: $email\n';
-  $message = 'A visitor to your site has sent the following email address to be added to your mailing list.\nName: $name\nEmail Address: $email';
+  $message = 'A visitor to your site has sent the following email address to be added to your mailing list.\n
+              Name: $name\n
+              Email Address: $email';
   mail($to,$subject,$message,$headers);
 ?>
